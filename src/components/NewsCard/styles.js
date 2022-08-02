c9c2e718630353a95812e2a620dd2c52ae9f3ab3
pankaj garad <pankajgarad73@gmail.com>
2022-08-02 20:15:14 +0530

    first commit

---
+++ src/components/NewsCard/styles.js
@@ -0,0 +1,79 @@
+// import { makeStyles } from "@mui/material";
+
+// export default makeStyles({
+//     media:{
+//         height:250,
+//     },
+//     border:{
+//         border:"solid",
+//     },
+//     card:{
+//         display:"flex",
+//         flexDirection:"column",
+//         justifyContent:'space-between',
+//         borderBottom:"10px solid white",
+//     },
+//     activeCard:{
+//         borderBottom:"10px solid #22289a",
+//     },
+//     grid:{
+//         display:"flex",
+//     },
+//     details:{
+//         display:"flex",
+//         justifyContent:"space-between",
+//         margin:"20px",
+//     },
+//     title:{
+//         padding:"0 16px",
+//     },
+//     cardActions:{
+//         padding:"0 16px 8 px 16px",
+//         display:"flex",
+//         justifyContent:"space-between",
+//     },
+// });
+
+
+
+
+
+
+import { makeStyles } from "@mui/styles";
+
+export default makeStyles({
+  media: {
+    height: 250,
+  },
+  border: {
+    border: "solid",
+  },
+  fullHeightCard: {
+    height: "100%",
+  },
+  card: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    borderBottom: "10px solid white",
+  },
+  activeCard: {
+    borderBottom: "10px solid #22289a",
+  },
+  grid: {
+    display: "flex",
+  },
+  details: {
+    display: "flex",
+    justifyContent: "space-between",
+    margin: "20px",
+  },
+  title: {
+    padding: "0 16px",
+  },
+  cardActions: {
+    padding: "0 16px 8px 16px",
+    display: "flex",
+    justifyContent: "space-between",
+  },
+});
